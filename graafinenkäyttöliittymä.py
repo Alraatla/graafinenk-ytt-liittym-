43407739b4917e054c47de023857406585971138
leinomk <38618113+leinomk@users.noreply.github.com>
2018-04-22 16:35:23 +0300

    Update graafinenkäyttöliittymä.py

--- graafinenkäyttöliittymä.py
+++ graafinenkäyttöliittymä.py
@@ -16,6 +16,8 @@ ALPHABET = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "å", "a", "s",
             "d", "f", "g", "h", "j", "k", "l", "ö", "ä", "z", "x", "c", "v",
             "b", "n", "m"]
 
+END_POINTS = 10
+
 
 class HangmanGame:
     def __init__(self):
@@ -60,7 +62,7 @@ class HangmanGame:
     def initialize_game(self):
         self.__turn = PLAYER1
         self.__pictureLabel.configure(image=self.__hangmanpics[self.__mistakes])
-        self.__word_to_guess = self.__word_entry.get()
+        self.__word_to_guess = self.__word_entry.get().lower()
         self.__word_entry.delete(0, END)
         self.__word_entry.insert(0, "*" * len(self.__word_to_guess))
         self.__word_entry.configure(state=DISABLED)
